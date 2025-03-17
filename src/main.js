f4946a0295056d06c4e45dea48ef797e241e4c4c
ecarcomo <emmanuelcarcomo@gmail.com>
2025-03-17 20:23:18 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-const { Client ,LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 
 // Load the dialog tree
 const conversationTree = JSON.parse(fs.readFileSync('./trees/dialog_tree.json', 'utf8'));
 // Load the user states
-const userStates = {}; 
+const userStates = {};
+// Número del representante con código de país
 
+const representanteId = "5491137658523@c.us";
 
 // Create a new client instance
 const client = new Client({
@@ -17,7 +19,7 @@ const client = new Client({
 // When the client received QR-Code
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
 // When the client is ready, run this code (only once)
@@ -25,87 +27,103 @@ client.once('ready', () => {
     console.log('Client is ready!');
 });
 
-
-
 // Listening to all incoming messages
-client.on('message',async message => {
-    const   chatId = message.from;
-    const   text = message.body.trim();
+client.on('message', async message => {
+    try {
+        const chatId = message.from;
+        let text = message.body ? message.body.trim() : '';
 
-    // Variables para controlar el flujo de la conversación
-    let     inichat = false;
-    let     noValid = '';
+        // Validar si el texto es solo una cadena de caracteres
+        if (validarTexto(text)) {
+            text = message.body.trim();
+        } else {
+            text = '';
+        }
 
-    if (!chatId.includes('33727415')) {
-        return ;
-    }
+        // Variables para controlar el flujo de la conversación
+        let inichat = false;
+        let noValid = '';
 
-    if (!userStates[chatId]) {
-        userStates[chatId] = "inicio";
-        inichat = true;
-    }
-    else {
-        inichat = false;
-    }
+        console.log(`Received message from ${chatId}: ${text}`);
 
-    if(text) {
-        console.log('Nodo Actual: ' + userStates[chatId]);
-        console.log(`[${chatId}] -> "${text}"`);
-        console.log('-----------------------------------');
-    }
-    
-
-    const currentNode = conversationTree[userStates[chatId]];
-
-    if (currentNode) {
-    
-        if (currentNode.representante) { // Reiniciar conversación después de un nodo final
+        if (!chatId.includes('60941390')) {
             return;
         }
 
-        if (currentNode.final) {
-            delete userStates[chatId]; // Reiniciar conversación después de un nodo final
-            return;
+        if (!userStates[chatId]) {
+            userStates[chatId] = "inicio";
+            inichat = true;
+        } else {
+            inichat = false;
         }
-    
-        if (currentNode.siguientes ) {
-            if(currentNode.siguientes[text]){
-                if(conversationTree[currentNode.siguientes[text]]){
-                   if(!inichat) userStates[chatId] = currentNode.siguientes[text];
-                }
-                else {
-                    noValid = 'Lo siento, ha ocurrido un error';
-                }
-            } 
-            else {
-                if(userStates[chatId] !== "inicio"){
-                    noValid = 'Opción no válida, por favor intenta de nuevo';
-                }
-                else if((userStates[chatId] === "inicio" && !inichat)){
-                    noValid = 'Opción no válida, por favor intenta de nuevo';
+
+        if (text) {
+            console.log('Nodo Actual: ' + userStates[chatId]);
+            console.log(`[${chatId}] -> "${text}"`);
+            console.log('-----------------------------------');
+        } else {
+            console.log('Message body is empty or null');
+        }
+
+        const currentNode = conversationTree[userStates[chatId]];
+
+        if (currentNode) {
+            if (currentNode.representante) {
+                return;
+            }
+
+            if (currentNode.final) {
+                delete userStates[chatId]; // Reiniciar conversación después de un nodo final
+                return;
+            }
+
+            if (currentNode.siguientes) {
+                if (currentNode.siguientes[text]) {
+                    if (conversationTree[currentNode.siguientes[text]]) {
+                        if (!inichat) userStates[chatId] = currentNode.siguientes[text];
+                    } else {
+                        noValid = 'Lo siento, ha ocurrido un error';
+                    }
+                } else {
+                    if (userStates[chatId] !== "inicio") {
+                        noValid = 'Opción no válida, por favor intenta de nuevo';
+                    } else if ((userStates[chatId] === "inicio" && !inichat)) {
+                        noValid = 'Opción no válida, por favor intenta de nuevo';
+                    }
                 }
             }
         }
-    }
 
-    
+        const newNode = conversationTree[userStates[chatId]];
 
-    const newNode = conversationTree[userStates[chatId]];
-
-    let response = (    noValid!==''    ?
-                        `\`\`\`${noValid}\`\`\`\n\n*${newNode.mensaje}*\n`
-                        :
-                        `*${newNode.mensaje}*\n`
-                    );
-
-    if (newNode.opciones) {
-        for (const [key, value] of Object.entries(newNode.opciones)) {
-            response += `\n${key}. ${value}`;
+        if (newNode.representante) {
+            await client.sendMessage(representanteId, `📢 *Nueva solicitud de atención* 📢\n\nEnlace: https://wa.me/${chatId.replace('@c.us', '')}\nMensaje: "${text}"`);
         }
-    }
 
-    await message.reply(response);
+        let response = (noValid !== '' ?
+            `\`\`\`${noValid}\`\`\`\n\n*${newNode.mensaje}*\n` :
+            `*${newNode.mensaje}*\n`
+        );
+
+        if (newNode.opciones) {
+            for (const [key, value] of Object.entries(newNode.opciones)) {
+                response += `\n${key}. ${value}`;
+            }
+        }
+
+        await message.reply(response);
+    } catch (error) {
+        console.error('Error processing message:', error);
+    }
 });
 
 // Start your client
 client.initialize();
+
+function validarTexto(text) {
+    // Validar si el texto es un contacto
+    if (text.includes('BEGIN:VCARD') || text.includes('END:VCARD'))
+        return false;
+
+    return true;
+}
