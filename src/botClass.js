85a857c101dd8d0ee960670f6b31bd22fea81d5f
ecarcomo <emmanuelcarcomo@gmail.com>
2025-03-20 19:16:24 -0300

    upgrade multiples instancias

---
+++ src/botClass.js
@@ -0,0 +1,197 @@
+// Import the required libraries
+import qrcode from 'qrcode-terminal';
+import fs from 'fs';
+import pkg from 'whatsapp-web.js';
+const { Client, LocalAuth } = pkg;
+
+
+
+class Bot {
+    constructor(nameBot,representanteId) {
+        this.nameBot = nameBot;
+        this.client = new Client({
+            authStrategy: new LocalAuth({ clientId:  this.nameBot }),
+            puppeteer: { headless: true }
+        });
+        this.userStates = {};
+        this.representanteId = representanteId;
+        this.conversationTree = JSON.parse(fs.readFileSync(`./trees/${this.nameBot}_dialog_tree.json`, 'utf8'));
+
+        // When the client received QR-Code
+        this.client.on('qr', (qr) => {
+            console.log(`QR Bot:"${this.nameBot}" RECEIVED`, qr);
+            qrcode.generate(qr, { small: true });
+        });
+
+        // When the client is ready, run this code (only once)
+        this.client.once('ready', () => {
+            console.log(`BOT ${this.nameBot} is ready!`);
+        });
+
+        // Listening to all incoming messages
+        this.client.on('message_create', async message => {
+            try {
+                const chatId = message.from;
+                let text = message.body ? message.body.trim() : '';
+
+                // Validar si el texto es solo una cadena de caracteres
+                if (this.validarTexto(text)) {
+                    text = message.body.trim();
+                } else {
+                    text = '';
+                }
+
+                // Variables para controlar el flujo de la conversación
+                let inichat = false;
+                let noValid = '';
+
+                //console.log(`Received message from ${chatId}: ${text}`);
+
+                if (!chatId.includes('63522348') && !chatId.includes('60941390') && !chatId.includes('5551642209')) {
+                    return;
+                }
+
+                if (!this.userStates[chatId]) {
+                    this.userStates[chatId] = { state: "inicio", lastmsj: Date.now().toString() ,data:{}};
+                    inichat = true;
+                } else {
+                    inichat = false;
+                }
+
+                
+
+                /*if (text) {
+                    console.log('Nodo Actual: ' + userStates[chatId].state);
+                    console.log(`[${chatId}] -> "${text}"`);
+                    console.log('-----------------------------------');
+                } else {
+                    console.log('Message body is empty or null');
+                }*/
+                // Actualizar date de último mensaje
+                this.userStates[chatId].lastmsj = Date.now().toString();
+
+
+                const currentNode = this.conversationTree[this.userStates[chatId].state];
+
+                // Validaciones de flujo de la conversación
+                if (currentNode) {
+
+                    if (currentNode.representante) {
+                    }
+                    else if (currentNode.final) {
+                        delete this.userStates[chatId]; // Reiniciar conversación después de un nodo final
+                    }
+                    else if (currentNode.siguientes) {
+                        if (currentNode.siguientes[text]) {
+                            if (this.conversationTree[currentNode.siguientes[text]]) {
+                                if (!inichat) this.userStates[chatId].state = currentNode.siguientes[text];
+                            } else {
+                                noValid = 'Lo siento, ha ocurrido un error';
+                            }
+                        } else {
+                            if (this.userStates[chatId].state !== "inicio") {
+                                noValid = 'Opción no válida, por favor intenta de nuevo';
+                            } else if ((this.userStates[chatId].state === "inicio" && !inichat)) {
+                                noValid = 'Opción no válida, por favor intenta de nuevo';
+                            }
+                        }
+                    }
+                }
+
+                console.log(`BOT ->${this.nameBot} | ChatId: ${chatId} | `,this.userStates[chatId]);
+                console.log('-----------------------------------');
+
+                if (currentNode.representante || currentNode.final) {
+                    return;
+                }
+
+                //Nuevo nodo de la conversación
+                const newNode = this.conversationTree[this.userStates[chatId].state];
+                //Si el nodo tiene representante, enviar mensaje al representante
+                if (newNode.representante) {
+                    await this.client.sendMessage(this.representanteId, `📢 *Nueva solicitud de atención* 📢\n\nEnlace: https://wa.me/${chatId.replace('@c.us', '')}`);
+                }
+
+                let response = (noValid !== '' ?
+                    `\`\`\`${noValid}\`\`\`\n\n${newNode.mensaje}\n` :
+                    `${newNode.mensaje}\n`
+                );
+
+                if (newNode.opciones) {
+                    for (const [key, value] of Object.entries(newNode.opciones)) {
+                        response += `\n${key}. ${value}`;
+                    }
+                }
+
+                await this.client.sendMessage(chatId,response);
+            } catch (error) {
+                console.error('Error processing message:', error);
+            }
+        });
+
+
+        // Listen for messages from the representative
+        this.client.on('message_create', async message => {
+            try {
+                if (message.from === representanteId) {
+                    const text = message.body.trim().toLowerCase();
+
+                    // Check if the message contains "adios" or "hasta luego"
+                    if (text.startsWith('adios') || text.startsWith('hasta luego')) {
+                        const chatIdToReset = message.to;
+                        if (this.userStates[chatIdToReset]) {
+                            this.userStates[chatIdToReset].state = 'fin';
+                            const msg = this.conversationTree[this.userStates[chatIdToReset].state].mensaje;
+                            await this.client.sendMessage(chatIdToReset, msg);
+                            delete this.userStates[chatIdToReset];
+                            await this.client.sendMessage(chatIdToReset, `\`\`\`Se ha finalizado la conversación.\`\`\``);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error('Error processing representative message:', error);
+            }
+        });
+    
+    }
+
+    destructor() {
+        // Add destructor logic here if needed
+    }
+
+    stop(){
+        // Add destructor logic here if needed
+        this.client.logout();
+        this.client.destroy();
+    }
+    // Function to clean up old user states
+    async cleanUpUserStates() {
+        await this.client.sendMessage(chatId, `\`\`\`Se cierra la conversación por inactividad. Si necesitas ayuda, por favor vuelve a escribirnos.\`\`\``);
+        const THIRTY_MINUTES = 30 * 60 * 1000;
+
+        for (const chatId in this.userStates) {
+            if (now - parseInt(this.userStates[chatId].lastmsj) > THIRTY_MINUTES) {
+                delete userStates[chatId];
+                console.log(`Estado del usuario ${chatId} eliminado por inactividad.`);
+                await this.client.sendMessage(chatId, `\`\`\`Se cierra la conversación por inactividad. Si necesitas ayuda, por favor vuelve a escribirnos.\`\`\``);
+            }
+        }
+    }
+
+    validarTexto(text) {
+    // Validar si el texto es un contacto
+    if (text.includes('BEGIN:VCARD') || text.includes('END:VCARD'))
+        return false;
+    return true;
+    }
+
+    // Start your client
+    start(){
+        this.client.initialize();
+        // Set an interval to clean up old user states every hour
+        setInterval(()=>this.cleanUpUserStates(), 60 * 60 * 1000);
+    }
+
+}
+
+export default Bot;
--- src/main.js
+++ src/main.js
@@ -1,183 +1,65 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-const fs = require('fs');
+import Bot from './botClass.js';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-// Load the dialog tree
-const conversationTree = JSON.parse(fs.readFileSync('./trees/electrovision_dialog_tree.json', 'utf8'));
-// Load the user states
-const userStates = {};
-// Número del representante con código de país
+const app = express();
+const port = process.env.PORT || 3030;
 
-const representanteId = "5491137658523@c.us";
+app.use(bodyParser.json());
+const bots = {};
 
-// Create a new client instance
-const client = new Client({
-    authStrategy: new LocalAuth({ clientId: 'EmmaBot' }),
-    puppeteer: { headless: false }
-});
-
-// When the client received QR-Code
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, { small: true });
-});
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Client is ready!');
-});
-
-// Listening to all incoming messages
-client.on('message_create', async message => {
+//ejemplo : http://localhost:3030/startBot?nameBot=electrovision&representanteId=5491137658523@c.us
+app.get('/startBot', async (req, res) => {
     try {
-        const chatId = message.from;
-        let text = message.body ? message.body.trim() : '';
-
-        // Validar si el texto es solo una cadena de caracteres
-        if (validarTexto(text)) {
-            text = message.body.trim();
-        } else {
-            text = '';
+        const nameBot = req.query.nameBot;
+        const representanteId = req.query.representanteId;
+        if (!nameBot || !representanteId) {
+            return res.status(400).send('Missing nameBot or representanteId');
         }
-
-        // Variables para controlar el flujo de la conversación
-        let inichat = false;
-        let noValid = '';
-
-        //console.log(`Received message from ${chatId}: ${text}`);
-
-        if (!chatId.includes('5551642209') && !chatId.includes('60941390') && !chatId.includes('64267883')) {
-            return;
+        if (bots[nameBot]) {
+            return res.status(400).send('Bot already started');
         }
-
-        if (!userStates[chatId]) {
-            userStates[chatId] = { state: "inicio", lastmsj: Date.now().toString() ,data:{}};
-            inichat = true;
-        } else {
-            inichat = false;
-        }
-
-        
-
-        /*if (text) {
-            console.log('Nodo Actual: ' + userStates[chatId].state);
-            console.log(`[${chatId}] -> "${text}"`);
-            console.log('-----------------------------------');
-        } else {
-            console.log('Message body is empty or null');
-        }*/
-        // Actualizar date de último mensaje
-        userStates[chatId].lastmsj = Date.now().toString();
-
-
-        const currentNode = conversationTree[userStates[chatId].state];
-
-        // Validaciones de flujo de la conversación
-        if (currentNode) {
-
-            if (currentNode.representante) {
-            }
-            else if (currentNode.final) {
-                delete userStates[chatId]; // Reiniciar conversación después de un nodo final
-            }
-            else if (currentNode.siguientes) {
-                if (currentNode.siguientes[text]) {
-                    if (conversationTree[currentNode.siguientes[text]]) {
-                        if (!inichat) userStates[chatId].state = currentNode.siguientes[text];
-                    } else {
-                        noValid = 'Lo siento, ha ocurrido un error';
-                    }
-                } else {
-                    if (userStates[chatId].state !== "inicio") {
-                        noValid = 'Opción no válida, por favor intenta de nuevo';
-                    } else if ((userStates[chatId].state === "inicio" && !inichat)) {
-                        noValid = 'Opción no válida, por favor intenta de nuevo';
-                    }
-                }
-            }
-        }
-
-        console.log(chatId,userStates[chatId]);
-        console.log('-----------------------------------');
-
-        if (currentNode.representante || currentNode.final) {
-            return;
-        }
-
-        //Nuevo nodo de la conversación
-        const newNode = conversationTree[userStates[chatId].state];
-        //Si el nodo tiene representante, enviar mensaje al representante
-        if (newNode.representante) {
-            await client.sendMessage(representanteId, `📢 *Nueva solicitud de atención* 📢\n\nEnlace: https://wa.me/${chatId.replace('@c.us', '')}`);
-        }
-
-        let response = (noValid !== '' ?
-            `\`\`\`${noValid}\`\`\`\n\n${newNode.mensaje}\n` :
-            `${newNode.mensaje}\n`
-        );
-
-        if (newNode.opciones) {
-            for (const [key, value] of Object.entries(newNode.opciones)) {
-                response += `\n${key}. ${value}`;
-            }
-        }
-
-        await client.sendMessage(chatId,response);
+        const bot = new Bot(nameBot, representanteId);
+        await bot.start();
+        bots[nameBot] = bot;
+        res.status(200).send('Bot started successfully');
     } catch (error) {
-        console.error('Error processing message:', error);
+        res.status(500).send(`Error starting bot: ${error.message}`);
     }
 });
 
-
-// Listen for messages from the representative
-client.on('message_create', async message => {
+app.get('/closeBot', async (req, res) => {
     try {
-        if (message.from === representanteId) {
-            const text = message.body.trim().toLowerCase();
-
-            // Check if the message contains "adios" or "hasta luego"
-            if (text.startsWith('adios') || text.startsWith('hasta luego')) {
-                const chatIdToReset = message.to;
-                if (userStates[chatIdToReset]) {
-                    userStates[chatIdToReset].state = 'fin';
-                    const msg = conversationTree[userStates[chatIdToReset].state].mensaje;
-                    await client.sendMessage(chatIdToReset, msg);
-                    delete userStates[chatIdToReset];
-                    await client.sendMessage(chatIdToReset, `\`\`\`Se ha finalizado la conversación.\`\`\``);
-                }
-            }
+        const nameBot = req.query.nameBot;
+        if (!nameBot) {
+            return res.status(400).send('Missing nameBot');
         }
+        const bot = bots[nameBot];
+        if (!bot) {
+            return res.status(400).send('Bot not found');
+        }
+        await bot.stop();
+        delete bots[nameBot];
+        res.status(200).send('Bot closed successfully');
     } catch (error) {
-        console.error('Error processing representative message:', error);
+        res.status(500).send(`Error closing bot: ${error.message}`);
     }
 });
 
-
-// Function to clean up old user states
-async function cleanUpUserStates() {
-    const now = Date.now();
-    const THIRTY_MINUTES = 30 * 60 * 1000;
-
-    for (const chatId in userStates) {
-        if (now - parseInt(userStates[chatId].lastmsj) > THIRTY_MINUTES) {
-            delete userStates[chatId];
-            console.log(`Estado del usuario ${chatId} eliminado por inactividad.`);
-            await client.sendMessage(chatId, `\`\`\`Se cierra la conversación por inactividad. Si necesitas ayuda, por favor vuelve a escribirnos.\`\`\``);
-        }
+app.get('/listBots', (req, res) => {
+    try {
+        const activeBots = Object.keys(bots);
+        res.status(200).json({ activeBots });
+    } catch (error) {
+        res.status(500).send(`Error listing bots: ${error.message}`);
     }
-}
+});
 
-// Set an interval to clean up old user states every hour
-setInterval(cleanUpUserStates, 60 * 60 * 1000);
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
 
+//Instanciar el bot de electrovision con parametros de nombreBot y representanteId
+//const botElectrovision = new Bot('electrovision','5491137658523@c.us');
+//botElectrovision.start();
 
-// Start your client
-client.initialize();
-
-function validarTexto(text) {
-    // Validar si el texto es un contacto
-    if (text.includes('BEGIN:VCARD') || text.includes('END:VCARD'))
-        return false;
-
-    return true;
-}
